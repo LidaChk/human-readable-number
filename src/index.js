da9e6b1bc52a37abd466285c328e08a52a347a55
Lida Cherkasova <lvglazko@gmail.com>
2022-01-05 17:36:43 +0000

    feat: Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+ 
+  let dgs = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+  let tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    
+  if (n < 20) return dgs[n];
+  if (n < 100) return tens[Math.floor((n/10)-2)] + (n%10? " " + dgs[n%10]: "");
+  if (n < 1000) return dgs[Math.floor(n/100)] +" hundred" + (n%100 == 0? "": " " +  toReadable(n%100));    
 }
